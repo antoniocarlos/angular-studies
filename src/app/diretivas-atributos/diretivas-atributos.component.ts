3b719397a4a4b57f2aa483542e0c5a636f32e5ce
Antonio Carlos Santos Thiele <antoniocarloss60@gmail.com>
2022-12-26 22:08:06 -0300

    ng-template

--- src/app/diretivas-atributos/diretivas-atributos.component.ts
+++ src/app/diretivas-atributos/diretivas-atributos.component.ts
@@ -12,10 +12,19 @@ export class DiretivasAtributosComponent implements OnInit {
 
   public height: string = '100px';
 
+  public name: string = 'Teste';
+
+  public list: Array<{ name: string }> = [
+    { name: 'Teste 1' },
+    { name: 'Teste 2' },
+    { name: 'Teste 3' },
+    { name: 'Teste 4' },
+  ];
+
   ngOnInit(): void {
     setInterval(() => {
       this.active = !this.active;
-      this.height = this.height === '100px' ? '500px' : '100px';
+      this.height = this.height === '100px' ? '100px' : '100px';
     }, 1000)
   }
 }
